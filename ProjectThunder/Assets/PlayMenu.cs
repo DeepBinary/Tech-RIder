596db99bad1a0bb594409a1cb119dbd3dfabf94c
Deependra s <deependra.suryadevara@gmail.com>
2021-09-27 20:50:25 +0530

    removed duplicate project

---
+++ ProjectThunder/Assets/PlayMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMenu : MonoBehaviour
+{
+    public float animationtime;
+    public void OnEnable()
+    {
+        this.gameObject.GetComponent<CanvasGroup>().alpha = 0;
+        this.gameObject.GetComponent<CanvasGroup>().LeanAlpha(1, animationtime);
+        transform.localScale = new Vector3(0, 0, 0);
+        transform.LeanScale(new Vector3(1, 1, 0), animationtime).setEaseInOutExpo();
+    }
+
+    public void CloseMenu()
+    {
+        this.gameObject.GetComponent<CanvasGroup>().LeanAlpha(0, animationtime);
+        transform.LeanScale(new Vector3(0, 0, 0), animationtime).setEaseInOutExpo().setOnComplete(HideObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CloseMenu();
+        }
+    }
+
+    void HideObject()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ ProjectThunder/Assets/PlayMenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMenuItem : MonoBehaviour
+{
+    public float animationtime;
+    private void OnEnable()
+    {
+        transform.rotation = new Quaternion(0, 360, 0, 0);
+        transform.LeanRotateY(0, animationtime);
+    }
+
+    public void Close()
+    {
+        transform.LeanRotateY(360, animationtime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Close();
+        }
+    }
+}
