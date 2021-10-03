517d2fd11b2538c62256b9206f7e51772078a766
Deependra s <deependra.suryadevara@gmail.com>
2021-10-03 14:19:31 +0530

    downgraded version

---
+++ Project-Thunder/Assets/Scripts/Elements/Coin.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Coin : MonoBehaviour
+{
+    public int CoinsRewarded;
+    [HideInInspector]public Manager gamemanager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamemanager = FindObjectOfType<Manager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Coinstuff();
+        }
+    }
+
+    public void Coinstuff()
+    {
+        gamemanager.Score += CoinsRewarded;
+        Destroy(this.gameObject);
+    }
+}
