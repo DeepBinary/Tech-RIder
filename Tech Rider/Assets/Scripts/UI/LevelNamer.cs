bcd5fd9d3622ad551a6ec213fcdc40b31fd74f7f
DeepBinaryLord <72721134+deependra-suryadevara@users.noreply.github.com>
2021-12-01 16:26:04 +0530

    mistake

--- Tech Rider/Assets/Scripts/UI/LevelNamer.cs
+++ Tech Rider/Assets/Scripts/UI/LevelNamer.cs
@@ -1,10 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMpro;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelNamer : MonoBehaviour
 {
+    public GameObject[] buttons;
+
+    int numlevels = 3;
+
+    public void Start()
+    {
+        numlevels = buttons.Length;
+        for (int i = 0; i < numlevels; i++)
+        {
+            TextMeshProUGUI[] textperbutton = buttons[i].GetComponentsInChildren<TextMeshProUGUI>();
+            textperbutton[1].text = (i + 1).ToString();
+
+            buttons[i].GetComponent<Button>().onClick.AddListener(delegate () {
+
+                LoadLevel(i);
+
+            });
+        }
+    }
     public void LoadLevel(int index)
     {
         FindObjectOfType<LevelLoader>().LoadScene(index);
