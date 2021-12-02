cd4790605fa63382e66315a6e70cd824f8a7b3b6
DeepBinaryLord <72721134+deependra-suryadevara@users.noreply.github.com>
2021-12-02 17:12:24 +0530

    project-naming update

---
+++ Project-Thunder/Assets/AnimatedButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimatedButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float zoomedscale;
+    public float animationtime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        transform.LeanScale(new Vector3(zoomedscale, zoomedscale, 0), animationtime);
+    } 
+
+    public void OnPointerExit(PointerEventData data)
+    {
+
+    }
+}
